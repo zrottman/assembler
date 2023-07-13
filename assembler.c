c94936030ea7c63954ff7a59dc466915af4a2752
zrottman <zrottman@example.com>
2023-07-13 09:03:35 -0700

    Implement functionality to open file specified as argv and print contents to stdout

--- assembler.c
+++ assembler.c
@@ -4,12 +4,28 @@
 
 int main(int argc, char **argv)
 {
+    // exit if argc is not as expected
     if (argc != 2) {
         printf("usage: assembler <path>\n");
         return EXIT_FAILURE; // macro defined in stdlib.h
     }
 
+    FILE *f;
+    int c;
+
     printf("You're opening file %s\n", argv[1]);
+    f = fopen(argv[1], "r");
+
+    if (f == NULL)
+        printf("something went wrong\n");
+
+    while ((c = fgetc(f)) != EOF)
+    {
+        printf("%c", c);
+    }
+
+    fclose(f);
+
 
     return 0;
 }
