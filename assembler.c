1e564e521dd646c6f926dcd091961ada6aabcb4c
zrottman <zrottman@example.com>
2023-07-13 08:46:23 -0700

    First commit: Implement argv and argc functionality to accept .asm filename from command line

---
+++ assembler.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2) {
+        printf("usage: assembler <path>\n");
+        return EXIT_FAILURE; // macro defined in stdlib.h
+    }
+
+    printf("You're opening file %s\n", argv[1]);
+
+    return 0;
+}
